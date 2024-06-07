2047a1ddb8b2dd0c3646f3bdee7d4f61bd1d9db5
Yehtetaung158 <yha15380@gmail.com>
2024-06-08 00:42:47 +0630

    multiple upload and display photo

--- porject/gallery-process.php
+++ porject/gallery-process.php
@@ -8,10 +8,18 @@ $saveFolder = "photo";
 if (!is_dir($saveFolder)) {
     mkdir($saveFolder, 0777);
 };
-$ext = pathinfo($_FILES["upload"]["name"])["extension"];
-$saveFileName = $saveFolder . "/" . uniqid()."." . $ext;
+$error = false;
 
-if (move_uploaded_file($_FILES["upload"]["tmp_name"], $saveFileName)) {
-    header("Location:gallery.php");
-    // print_r($saveFileName);
-}
+foreach ($_FILES["upload"]["name"] as $key => $name) {
+    $ext = pathinfo($name)["extension"];
+    $saveFileName = $saveFolder . "/" . uniqid() . "." . $ext;
+
+    if (!move_uploaded_file($_FILES["upload"]["tmp_name"][$key], $saveFileName)) {
+        // print_r($saveFileName);
+        $error = true;
+    };
+};
+
+if($error===false){
+        header("Location:gallery.php");
+};
--- porject/gallery.php
+++ porject/gallery.php
@@ -1,10 +1,10 @@
 <?php include("./template/header.php") ?>
 <?php include("./template/side_bar.php") ?>
 <section class="max-w-sm">
-  <form enctype="multipart/form-data" method="post" action="./gallery-process.php">
-    <label class="block">
-      <span class="sr-only">Choose profile photo</span>
-      <input type="file" name="upload" class="block w-full text-sm text-gray-500
+    <form enctype="multipart/form-data" method="post" action="./gallery-process.php">
+        <label class="block">
+            <span class="sr-only">Choose profile photo</span>
+            <input type="file" name="upload[]" multiple class="block w-full text-sm text-gray-500
         file:me-4 file:py-2 file:px-4
         file:rounded-lg file:border-0
         file:text-sm file:font-semibold
@@ -15,10 +15,21 @@
         dark:file:bg-blue-500
         dark:hover:file:bg-blue-400
       ">
-    </label>
-    <button type="submit" class=" mt-8 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
-  Upload
-</button>
-  </form>
+        </label>
+        <button type="submit" class=" mt-8 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+            Upload
+        </button>
+    </form>
+
+    <?php
+    $photos = array_filter(scandir("photo"),fn($el)=>$el!= "." && $el!="..");
+    ?>
+
+    <div class=" columns-2" >
+        <?php foreach ($photos as $photo) : ?>
+            <img class=" mb-3" src="./photo/<?= $photo?>">
+        <?php endforeach ?>
+    </div>
+
 </section>
 <?php include("./template/footer.php") ?>
