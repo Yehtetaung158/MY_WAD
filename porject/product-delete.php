c80365be19d0712da4a26ac9ab8aedd52033f855
Yehtetaung158 <yha15380@gmail.com>
2024-06-10 00:20:21 +0630

    delete_done

--- porject/product-delete.php
+++ porject/product-delete.php
@@ -1,8 +1,14 @@
 <?php
 
-print_r($_GET);
+// print_r($_GET);
 
 $fileName=$_GET["file_name"];
 $content=file_get_contents("products/".$fileName);
 $obj=json_decode($content);
-print_r($obj);
+print_r($obj->product_photo);
+
+if(unlink($obj->product_photo)){
+    if(unlink("products/".$fileName)){
+        header("Location:product.php");
+    }
+}
--- porject/product.php
+++ porject/product.php
@@ -43,14 +43,14 @@
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200 flex">
                                         <!-- <?= $obj->product_rating ?> -->
                                         <?php for ($i = 1; $i <= 5; $i++) : ?>
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="" class="size-6 <?=$obj->product_rating <$i ? 'fill-gray-300 ' : 'fill-yellow-500'?>">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="" class="size-6 <?= $obj->product_rating < $i ? 'fill-gray-300 ' : 'fill-yellow-500' ?>">
                                                 <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
                                             </svg>
 
                                         <?php endfor ?>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                                        <a href="./product-delete.php?file_name=<?php $product ?>" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Delete</\a>
+                                        <a href="./product-delete.php?file_name=<?= $product ?>" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Delete</\a>
                                     </td>
                                 </tr>
                             <?php endforeach ?>
