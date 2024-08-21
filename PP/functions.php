124cabc80c589323a1194cdc54372a3379d6013e
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-08-21 21:32:17 +0630

    code from index to functin.php

--- PP/functions.php
+++ PP/functions.php
@@ -2,6 +2,28 @@
 
 function views(string $file): bool
 {
-    require_once "./views/".$file.".php";
+    require_once "./views/" . $file . ".php";
     return true;
 };
+
+function conrtoller(string $current): bool
+{
+    $arr = explode("@", $current);
+    $controllerfile = $arr[0];
+    $controllerFunction = $arr[1];
+    require_once "./controller/" . $controllerfile . ".php";
+    call_user_func($controllerFunction);
+    return true;
+}
+
+function routing($routes): void
+{
+    $path = $_SERVER['PATH_INFO'] ?? "/";
+    echo "<pre>";
+    $current = $routes[$path] ?? false;
+    if ($current) {
+        conrtoller($current);
+    } else {
+        views("not-found");
+    };
+}
--- PP/index.php
+++ PP/index.php
@@ -3,34 +3,24 @@
 require_once "./functions.php";
 require_once "./routes/web.php";
 
-$path = $_SERVER['PATH_INFO'] ?? "/";
+routing($routes);
 
-// if($path === '/'){
-//     require_once "./views/home.php";
-// }else if($path === "/about-us"){
-//     require_once "./views/about.php";
-// }else if($path === "/services"){
-//     require_once "./views/services.php";
-// }else {
-//     require_once "./views/not-found.php";
-// }
+// $path = $_SERVER['PATH_INFO'] ?? "/";
 
-// views($routes[$path] ?? "not-found");
+// echo "<pre>";
 
-echo "<pre>";
+// $current = $routes[$path] ?? false;
 
-$current = $routes[$path] ?? false;
+// if ($current) {
 
-if($current){
+//     $arr = explode("@", $current);
 
-    $arr = explode("@", $current);
+//     $controllerfile = $arr[0];
+//     $controllerFunction = $arr[1];
 
-    $controllerfile = $arr[0];
-    $controllerFunction = $arr[1];
-    
-    require_once "./controller/" . $controllerfile . ".php";
-    
-    call_user_func($controllerFunction);
-}else{
-    views("not-found");
-};
+//     require_once "./controller/" . $controllerfile . ".php";
+
+//     call_user_func($controllerFunction);
+// } else {
+//     views("not-found");
+// };
