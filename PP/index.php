4414dd41a940df62ed9924033e7fc406d80207b4
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-08-21 15:02:19 +0630

    route with array

--- PP/index.php
+++ PP/index.php
@@ -3,12 +3,20 @@
 echo("I am indexs");
 $path = $_SERVER['PATH_INFO'] ?? "/";
 
-if($path === '/'){
-    require_once "./views/home.php";
-}else if($path === "/about-us"){
-    require_once "./views/about.php";
-}else if($path === "/services"){
-    require_once "./views/services.php";
-}else {
-    require_once "./views/not-found.php";
-}
+// if($path === '/'){
+//     require_once "./views/home.php";
+// }else if($path === "/about-us"){
+//     require_once "./views/about.php";
+// }else if($path === "/services"){
+//     require_once "./views/services.php";
+// }else {
+//     require_once "./views/not-found.php";
+// }
+
+$routes=[
+    "/"=>"./views/home.php",
+    "/about-us"=>"./views/about.php",
+    "/services"=>"./views/services.php",
+];
+
+require_once $routes[$path] ?? "./views/not-found.php";
