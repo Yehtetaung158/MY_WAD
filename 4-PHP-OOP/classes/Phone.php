a7e311f0682078f204409a8f34e0ec7a0d29ec66
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-09-10 06:08:21 +0630

    constructor destract example

--- 4-PHP-OOP/classes/Phone.php
+++ 4-PHP-OOP/classes/Phone.php
@@ -1,11 +1,18 @@
 <?php
 
-echo "<pre>";
+// echo "<pre>";
 
 class Phone
 {
     public $name;
     public $model;
+
+    function __construct($inputName, $inputModel)
+    {
+        //    echo "I am counstructor"; 
+        $this->name = $inputName;
+        $this->model = $inputModel;
+    }
 
     public function makeCall()
     {
@@ -15,5 +22,10 @@ class Phone
     public function useingInternet()
     {
         return $this->model . "can use internet";
+    }
+
+    function __destruct()
+    {
+        echo "I am destructor";
     }
 }
--- 4-PHP-OOP/index.php
+++ 4-PHP-OOP/index.php
@@ -2,9 +2,10 @@
 
 require_once "./classes/Phone.php";
 
-$myPhone=new Phone;
+$myPhone=new Phone("SamSung","s24");
 
-$myPhone->name="Samsung";
-$myPhone->model="s24";
+// $myPhone->name="Samsung";
+// $myPhone->model="s24";
 echo $myPhone->makeCall();
+echo "<br>";
 echo $myPhone->useingInternet();
