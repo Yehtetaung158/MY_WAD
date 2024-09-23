55eae17636a7e082cef74168ff9da8055a28de17
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-09-24 06:23:21 +0630

    yield , stck , extends  are done

--- WADJuly/laravel-intro/resources/views/pages/about.blade.php
+++ WADJuly/laravel-intro/resources/views/pages/about.blade.php
@@ -1,3 +1,23 @@
 @extends('pages.main')
+@push('style')
+    <style>
+        .content {
+            background-color: yellow;
+        }
+    </style>
+@endpush
 <h1>I am About</h1>
-
+@section('content')
+    <div class="content">
+        <p>I am About.</p>
+        <p>I am About.</p>
+        <p>I am About.</p>
+        <p>I am About.</p>
+    </div>
+@endsection
+@push('script')
+    <p>I am script</p>
+    <script>
+        alert("I am script")
+    </script>
+@endpush
--- WADJuly/laravel-intro/resources/views/pages/contact.blade.php
+++ WADJuly/laravel-intro/resources/views/pages/contact.blade.php
@@ -1,2 +1,23 @@
 @extends('pages.main')
+@push('style')
+    <style>
+        .content {
+            background-color: green;
+        }
+    </style>
+@endpush
 <h1>I am Contact</h1>
+@section('content')
+    <div class="content">
+        <p>I am content.</p>
+        <p>I am content.</p>
+        <p>I am content.</p>
+        <p>I am content.</p>
+    </div>
+@endsection
+@push('script')
+    <p>I am script</p>
+    <script>
+        alert("I am script")
+    </script>
+@endpush
